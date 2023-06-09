fd2dc4d7e559c4c68734bdbb4da3726395562926
Jan Kędzierski <jkedzierski087@gmail.com>
2023-06-10 00:19:28 +0200

    add employees tests and departments small error in employees

---
+++ models/test/employee.test.js
@@ -0,0 +1,64 @@
+const Employee = require('../employee.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Employee', () => {
+  it('should throw an error if no "firstName", "lastName", "department" arg', () => {
+    const emp = new Employee({}); // create new Department, but don't set `name` attr value
+
+    emp.validate((err) => {
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    });
+
+    after(() => {
+      mongoose.models = {};
+    });
+  });
+
+  it('should throw an error if "firstName", "lastName", "department" is not a string', () => {
+    const cases = [{}, []];
+    for (let firstName of cases) {
+      const emp = new Employee({ firstName });
+
+      emp.validate((err) => {
+        expect(err.errors.firstName).to.exist;
+      });
+    }
+    for (let lastName of cases) {
+      const emp = new Employee({ lastName });
+
+      emp.validate((err) => {
+        expect(err.errors.lastName).to.exist;
+      });
+    }
+    for (let department of cases) {
+      const emp = new Employee({ department });
+
+      emp.validate((err) => {
+        expect(err.errors.department).to.exist;
+      });
+    }
+
+    after(() => {
+        mongoose.models = {};
+      });
+  });
+
+  it('should not throw an error for valid firstNames, lastNames, departments', () => {
+
+    const validCases = [{ firstName: 'maks', lastName: 'smith', department: 'IT'  }];
+
+    for (let example of validCases) {
+      const dep = new Employee({ example });
+  
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      });
+    }
+      after(() => {
+        mongoose.models = {};
+      });
+  });
+});
